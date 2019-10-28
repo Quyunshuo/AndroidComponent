d3b31e5582bcdb27ddc60ed569b6764b9a0a5f3c
Quyunshuo <quyunshuo@qq.com>
2019-10-28 11:24:48 +0800

    Modify[]:组件单独运行时Activity完善 添加各组件的Fragment

---
+++ other_module/src/main/java/com/quyunshuo/other_module/fragment/OtherFragment.java
@@ -0,0 +1,35 @@
+package com.quyunshuo.other_module.fragment;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.quyunshuo.other_module.R;
+
+public class OtherFragment extends Fragment {
+
+    public OtherFragment() {
+        // Required empty public constructor
+    }
+
+    public static OtherFragment newInstance() {
+        OtherFragment fragment = new OtherFragment();
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_other, container, false);
+    }
+}
